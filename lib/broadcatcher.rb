b0e44fb401b7f901aa58d4b29b492e5394f11410
dougsko <dougtko@gmail.com>
2009-12-02 06:06:18 -0500

    ...

--- lib/broadcatcher.rb
+++ lib/broadcatcher.rb
@@ -14,7 +14,7 @@ class Broadcatcher
     end
 
     def create_config
-        File.open("/home/doug/.broadcatcher/config.yml", 'w') do |f|
+        File.open(@config_file, 'w') do |f|
             config = { :working_dir => "/home/doug/.broadcatcher/",
                        :db_file => "/home/doug/.broadcatcher/downloaded.sqlite",
                        :log => "/home/doug/.broadcatcher/log.txt",
@@ -23,6 +23,7 @@ class Broadcatcher
                                    'save_dir' => '/home/doug/torrents',
                                    'regex_false' => 'mkv',
                                    'regex_true' => '.*',
+                                   'scan_time' => '60',
                                  }
                                 ]
             }
@@ -38,20 +39,33 @@ class Broadcatcher
 
     def download
         @config[:feeds].each do |feed|
-            Thread.new{
-                rss_feed = feed["url"]
-                rss_content = ""
-                open(rss_feed) do |f|
-                    rss_content = f.read
+            #Thread.new{
+                while true do
+                    rss_feed = feed["url"]
+                    rss_content = ""
+                    open(rss_feed) do |f|
+                        rss_content = f.read
+                    end
+                    rss = RSS::Parser.parse(rss_content, false)
+                    rss.channel.items.each do |item|
+                        url = URI.parse(item.enclosure.url)
+                        url.open do |file_to_get|
+                            if item.description[/Filename: (.*);/]
+                                file_name = "#{$1.gsub(/ /, '_').gsub(/,/, '')}.torrent"
+                            else
+                                url.path.match(/\/([^\/]*)$/)
+                                file_name = $1.gsub(/ /, '_').gsub(/,/, '')
+                            end
+                            File.open("#{feed['save_dir']}/#{file_name}", 'w') do |file|
+                                file << file_to_get.read
+                            end
+                        end
+                    end
+                    sleep feed["scan_time"]
                 end
-
-                rss = RSS::Parser.parse(rss_content, false)
-                rss.channel.items.each do |item|
-                    puts item.title
-                end
-
-            }.join
+            #}.join
         end
     end
+
 end
 
