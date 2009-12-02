d0aff21eaf4d57e7ceb6ea23d56265e09fcafa3d
dougsko <dougtko@gmail.com>
2009-12-02 04:31:17 -0500

    got things organized

--- lib/broadcatcher.rb
+++ lib/broadcatcher.rb
@@ -4,10 +4,54 @@
 # Handles downloading of files and checking for dups.
 #
 
-require 'feed'
+require 'yaml'
+require 'rss'
+require 'open-uri'
 
 class Broadcatcher
-    def intitialize
+    def initialize
+        @config_file = "/home/doug/.broadcatcher/config.yml"
     end
 
+    def create_config
+        File.open("/home/doug/.broadcatcher/config.yml", 'w') do |f|
+            config = { :working_dir => "/home/doug/.broadcatcher/",
+                       :db_file => "/home/doug/.broadcatcher/downloaded.sqlite",
+                       :log => "/home/doug/.broadcatcher/log.txt",
+                       :feeds => [{'name' => 'example feed',
+                                   'url' => 'http://foo',
+                                   'save_dir' => '/home/doug/torrents',
+                                   'regex_false' => 'mkv',
+                                   'regex_true' => '.*',
+                                 }
+                                ]
+            }
+        f.puts config.to_yaml  
+        end
+    end
+
+    def read_config
+        File.open(@config_file, 'r') do |f|
+            @config = YAML.load(f)
+        end
+    end
+
+    def download
+        @config[:feeds].each do |feed|
+            Thread.new{
+                rss_feed = feed["url"]
+                rss_content = ""
+                open(rss_feed) do |f|
+                    rss_content = f.read
+                end
+
+                rss = RSS::Parser.parse(rss_content, false)
+                rss.channel.items.each do |item|
+                    puts item.title
+                end
+
+            }.join
+        end
+    end
 end
+
--- spec/broadcatcher_spec.rb
+++ spec/broadcatcher_spec.rb
@@ -1,7 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Broadcatcher" do
-  it "new broadcatcher" do
-    broad = Broadcatcher.new
+  before do
+      @bc = Broadcatcher.new
   end
+
+  it "new broadcatcher" do
+    @bc.class.to_s.should match(/Broadcatcher/)
+  end
+
+  it "creates a config file" do
+      @bc.create_config
+      a = File.exists? "#{ENV['HOME']}/.broadcatcher/config.yml"
+      a.to_s.should match(/true/)
+  end
+
+
 end
